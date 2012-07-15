fd55ad5956ad6826b67b329278a8ec2de5ff0605
Alexandur Marinescu <almarinescu@gmail.com>
2012-07-15 14:35:11 +0300

    Initial leingein project.

---
+++ LSM-tree/src/lsmt/core.clj
@@ -0,0 +1,6 @@
+(ns LSM-tree-1.core)
+
+(defn -main
+  "I don't do a whole lot."
+  [& args]
+  (println "Hello, World!"))
---
+++ LSM-tree/test/lsmt/core_test.clj
@@ -0,0 +1,7 @@
+(ns LSM-tree-1.core-test
+  (:use clojure.test
+        LSM-tree-1.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
